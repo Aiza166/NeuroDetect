5c492a2b1be074ca5a698e68442ae8718449597e
Aiza Gazyani <167635394+Aiza166@users.noreply.github.com>
2025-07-06 14:42:26 +0500

    Update tailwind.config.js

--- neurodetect-frontend/tailwind.config.js
+++ neurodetect-frontend/tailwind.config.js
@@ -1,1 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
